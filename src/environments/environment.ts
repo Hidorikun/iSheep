24b36e2281b5227f7d1aaa8ba1b4a2d8e05f3a53
George <georgevele2016@gmail.com>
2019-10-06 19:17:07 +0300

    Added firebase support

---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyDQOO4oFdRgjIYDT8iV-CQFkGp4qsGpOLw',
+    authDomain: 'isheep-ubb.firebaseapp.com',
+    databaseURL: 'https://isheep-ubb.firebaseio.com',
+    projectId: 'isheep-ubb',
+    storageBucket: '',
+    messagingSenderId: '361836912644',
+    appId: '1:361836912644:web:bc100706d59de5554c5551'
+  }
+};
